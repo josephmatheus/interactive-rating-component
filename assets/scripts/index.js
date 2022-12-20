a3f9120ba91dbeea5c0c2fa186551233f670de0b
Joseph Matheus <josephmatheus@hotmail.com>
2022-12-20 14:28:43 -0300

    Add checked style in rates button

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,8 +1,9 @@
+const labels = document.querySelectorAll("label");
 const submitBtn = document.getElementById("submitBtn");
 
 submitBtn.addEventListener("click", () => {
   const rate = document.querySelector("input[name='rating']:checked").value;
-  
+
   const thanksContainer = document.querySelector(".thanks-container");
   const ratingContainer = document.querySelector(".rating-container");
   const res = document.getElementById("res");
@@ -11,3 +12,11 @@ submitBtn.addEventListener("click", () => {
   ratingContainer.classList.add("hidden");
   res.innerText = `You selected ${rate} out of 5`;
 });
+
+labels.forEach(e => {
+  e.addEventListener("click", () => {
+    const rates = document.querySelectorAll("input[name='rating']");
+    e.style.backgroundColor = "rgb(124, 135, 152)";
+    e.style.color = "white";
+  })
+})
